61e46b5667ca3fc4f0f884da12bceb068c039c58
wayney <wayney1997@gmail.com>
2019-04-04 13:02:10 +0900

    fixed weird parts

--- newton_eval.m
+++ newton_eval.m
@@ -1,10 +1,8 @@
 function y=newton_eval(a,p,x) % a p : vector , x : scalar
-n=size(a,2);
 
-q=a(n);
-%Horner method
-for k = n-1:-1:1 
-    q = q*(x-p(k))+a(k);
+y=a(size(a,2));
+%Horner method using nested forms 
+
+for k = size(a,2)-1:-1:1 
+    y = y*(x-p(k))+a(k);
 end
-
-y=q;
--- test.m
+++ test.m
@@ -1,8 +1,9 @@
 x=5/2;
 c=log(1);
 % getting coefficients by Newton forms
-c=[c -(newton_eval(c,0,x)-log(2))/(2-1)];
-c=[c -(newton_eval(c,1,x)-log(3))/(3-1)/(3-2)];
-c=[c -(newton_eval(c,[1 2],x)-log(4))/(4-1)/(4-2)/(4-3)];
+c=[c -(newton_eval(c,0,2)-log(2))/(2-1)];
+c=[c -(newton_eval(c,1,3)-log(3))/(3-1)/(3-2)];
+c=[c -(newton_eval(c,[1 2],4)-log(4))/(4-1)/(4-2)/(4-3)];
 
 newton_eval(c,[1 2 3],x)
+
